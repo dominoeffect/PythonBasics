263180ab12288d395e727762e98458838083c13f
Li Hai <dominoleeradio@gmail.com>
2019-10-12 14:19:26 +0800

    tcp socket
    create tcp socket

---
+++ network/psocket.py
@@ -0,0 +1,26 @@
+# 导入socket库:
+import socket
+
+# 创建一个socket:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('www.baidu.com', 80))
+# 要求返回首页内容
+s.send(b'GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n')
+# 接收数据:
+buffer = []
+while True:
+    # 每次最多接收1k字节:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = b''.join(buffer)
+s.close()
+# print(data)
+header, html = data.split(b'\r\n\r\n', 1)
+print(header.decode('utf-8'))
+# 把接收的数据写入文件:
+with open('baidu.html', 'wb') as f:
+    f.write(html)
